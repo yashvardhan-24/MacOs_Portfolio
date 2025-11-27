235b1a145104ef8c4dbf107aea2807eab809d148
yashvardhan-24 <yash.vardhan2023@glbajajgroup.org>
2025-11-28 01:16:45 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+          "#components": resolve(dirname(fileURLToPath(import.meta.url)), "src/components"),
+          "#constants": resolve(dirname(fileURLToPath(import.meta.url)), "src/constants"),
+          "#store": resolve(dirname(fileURLToPath(import.meta.url)), "src/store"),
+          "#hoc": resolve(dirname(fileURLToPath(import.meta.url)), "src/hoc"),
+          "#window": resolve(dirname(fileURLToPath(import.meta.url)), "src/windows"),
+
+      }
+    }
+})
